18056053a94b3a8524674016cd01aef0d483fbb4
Griffin Faecher <griffin.faecher@tufts.edu>
2025-10-08 14:54:08 -0400

    Using the ledc functions to make a square wave

--- seniorDesign/src/main.cpp
+++ seniorDesign/src/main.cpp
@@ -11,39 +11,27 @@
 #include "pinout_defines.h"
 #include "wheel.h"
 
+int freq = 2000;
+const int ledChannel = 0;
+const int resolution = 8;
 void driveForward(Wheel wheel);
 
 /* MAIN */
 void loop() {
-    Wheel wheel_three;
-    setup();
-    driveForward(wheel_three);
+
 }
 
 void setup() {
-    pinMode(WHEEL3_PULSE_LIFT, OUTPUT);
-    pinMode(WHEEL3_PULSE_SWERVE, OUTPUT);
-    pinMode(WHEEL3_PULSE_DRIVE, OUTPUT);
-    
-    pinMode(WHEEL3_ENABLE_LIFT, OUTPUT);
-    pinMode(WHEEL3_ENABLE_SWERVE, OUTPUT);
-    pinMode(WHEEL3_ENABLE_DRIVE, OUTPUT);
+    pinMode(1, OUTPUT);
 
-    pinMode(WHEEL3_DIRECTION_LIFT, OUTPUT);
-    pinMode(WHEEL3_DIRECTION_SWERVE, OUTPUT);
-    pinMode(WHEEL3_DIRECTION_DRIVE, OUTPUT);
+    ledcSetup(ledChannel, freq, resolution);
+    ledcAttachPin(1, ledChannel);
+    ledcWrite(ledChannel, 128);
 
 }
 
 /* Move given wheel forwards */
 void driveForward(Wheel wheel) {
-    /* Set pins for drive motor */
-    wheel.enable(wheel.drive);
-    // wheel.setPulseHigh(wheel.drive);
-    wheel.setDirHigh(wheel.drive);
-    // 240,000,000 / 4,000 = 60,000
-    int frequency = 60;
-    wheel.testPulseSignal(wheel.drive, frequency);
 
 }
 
