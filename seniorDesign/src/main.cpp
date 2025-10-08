228c8843d5fa27cf2c13248642333851c3b55a04
Griffin Faecher <griffin.faecher@tufts.edu>
2025-10-08 17:17:35 -0400

    Square wave finally oscillates, just really slow and clunky

--- seniorDesign/src/main.cpp
+++ seniorDesign/src/main.cpp
@@ -10,23 +10,39 @@
 #include <Arduino.h>
 #include "pinout_defines.h"
 #include "wheel.h"
+#include "driver/ledc.h"
 
+const int pin = 1;
 int freq = 2000;
 const int ledChannel = 0;
 const int resolution = 8;
+const int duty = (1 << resolution) / 2;
 void driveForward(Wheel wheel);
 
 /* MAIN */
 void loop() {
+    for(int i = 1000; i > 0; i-=2) {
+        analogWriteFrequency(pin, freq--);
+        delay(10);
+    }
+    delay(500);
+    for(int i = 0; i < 1000; i+=2) {
+        analogWriteFrequency(pin, freq++);
+        delay(10);
+    }
+    delay(500);
 
 }
 
 void setup() {
-    pinMode(1, OUTPUT);
+    Serial.begin(115200);
+    pinMode(pin, OUTPUT);
 
-    ledcSetup(ledChannel, freq, resolution);
-    ledcAttachPin(1, ledChannel);
-    ledcWrite(ledChannel, 128);
+    /* LOOK INTO WHAT THE APB CLOCK ACTUALLY IS. IT'S DIVIDED FROM THE MAIN
+    80MHZ CLK, BUT TO WHAT? */
+    // if (!ledcSetClockSource(LEDC_USE_REF_TICK)) printf("Failure Configuring LEDC Source Clock.\n");
+    if (!ledcAttachChannel(pin, freq, resolution, 1)) printf("Failure Setting up pin %d channel.\n", pin);
+    if (!ledcWrite(pin, duty)) printf("Failure Writing to Pin.\n");
 
 }
 
