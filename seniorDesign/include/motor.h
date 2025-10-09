1b52cae1cde4c47af263a9fe78108832a75af869
Griffin Faecher <griffin.faecher@tufts.edu>
2025-10-08 22:56:38 -0400

    motor.cpp and motor.h created, and a shit ton of documentation on StepperEngine

---
+++ seniorDesign/include/motor.h
@@ -0,0 +1,29 @@
+/* motor.h
+ * Purpose: Motor Class
+ */
+
+#ifndef _MOTOR
+#define _MOTOR
+
+#include <stdint.h>
+#include "pinout_defines.h"
+
+
+class Motor {
+public:
+        Motor(uint8_t step_pin, uint8_t enable_pin, uint8_t swerve_pin);
+        ~Motor();
+
+
+
+       
+
+        
+
+private:
+
+        int step_pin, enable_pin, swerve_pin;
+
+};
+
+#endif
--- seniorDesign/src/main.cpp
+++ seniorDesign/src/main.cpp
@@ -11,50 +11,16 @@
 #include "pinout_defines.h"
 #include "wheel.h"
 #include "driver/ledc.h"
+#include "FastAccelStepper.h"
 
-const int pin = 1;
-int freq = 2000;
-const int ledChannel = 0;
-const int resolution = 8;
-const int duty = (1 << resolution) / 2;
-void driveForward(Wheel wheel);
+void setup() {
+
+
+
+}
 
 /* MAIN */
 void loop() {
-    for(int i = 1000; i > 0; i-=2) {
-        analogWriteFrequency(pin, freq--);
-        delay(10);
-    }
-    delay(500);
-    for(int i = 0; i < 1000; i+=2) {
-        analogWriteFrequency(pin, freq++);
-        delay(10);
-    }
-    delay(500);
 
 }
 
-void setup() {
-    Serial.begin(115200);
-    pinMode(pin, OUTPUT);
-
-    /* LOOK INTO WHAT THE APB CLOCK ACTUALLY IS. IT'S DIVIDED FROM THE MAIN
-    80MHZ CLK, BUT TO WHAT? */
-    // if (!ledcSetClockSource(LEDC_USE_REF_TICK)) printf("Failure Configuring LEDC Source Clock.\n");
-    if (!ledcAttachChannel(pin, freq, resolution, 1)) printf("Failure Setting up pin %d channel.\n", pin);
-    if (!ledcWrite(pin, duty)) printf("Failure Writing to Pin.\n");
-
-}
-
-/* Move given wheel forwards */
-void driveForward(Wheel wheel) {
-
-}
-
-void lift() {
-
-}
-
-void turn(){
-    
-}
